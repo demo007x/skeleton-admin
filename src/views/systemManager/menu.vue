d70f8d287baf9a798d16c87f97aff398b627b688
anziguoer <anziguoer@163.com>
2020-09-21 08:39:33 +0800

    优化菜单功能

--- src/views/systemManager/menu.vue
+++ src/views/systemManager/menu.vue
@@ -18,17 +18,17 @@
           <div slot="header" class="clearfix">
             <span>菜单列表</span>
           </div>
-          <el-tree ref="tree" class="filter-tree" default-expand-all :filter-node-method="filterNode" :data="list" :props="defaultProps" accordion :highlight-current="true" show-checkbox expand-on-click-node="false" @node-click="handleNodeClick">
+          <el-tree ref="tree" class="filter-tree" default-expand-all :filter-node-method="filterNode" :data="list" :props="defaultProps" accordion :highlight-current="true" :expand-on-click-node=false @node-click="handleNodeClick">
             <span slot-scope="{ node, data }" class="custom-tree-node">
               <span>{{ node.label }}</span>
               <span>
-                <el-button
-                  type="text"
-                  size="mini"
-                  style="color:#409EFF"
-                  icon="el-icon-edit"
-                  @click="() => editMenu(data)"
-                />
+<!--                <el-button-->
+                <!--                  type="text"-->
+                <!--                  size="mini"-->
+                <!--                  style="color:#409EFF"-->
+                <!--                  icon="el-icon-edit"-->
+                <!--                  @click="() => editMenu(data)"-->
+                <!--                />-->
                 <el-button
                   type="text"
                   size="mini"
@@ -99,180 +99,182 @@
 </template>
 
 <script>
-  import { createMenu, menuInfo, menusTree, updateMenu, destroyMenu } from '@/api/menu'
-  const menuQuery = {
-    name: ''
-  }
-  const defaultMenuData = {
-    id: 0,
-    pid: 0,
-    name: '',
-    title: '',
-    icon: '',
-    path: '',
-    component: 'Layout',
-    is_cache: 0,
-    sort: 1
-  }
-  export default {
-    name: 'Menus',
-    data() {
-      return {
-        menuQuery: Object.assign({}, menuQuery),
-        tableKey: 0,
-        list: [],
-        defaultProps: {
-          children: 'children',
-          label: 'title'
-        },
-        textMap: [
-          'create',
-          'update'
+import { createMenu, menuInfo, menusTree, updateMenu, destroyMenu } from '@/api/menu'
+const menuQuery = {
+  name: ''
+}
+const defaultMenuData = {
+  id: 0,
+  pid: 0,
+  name: '',
+  title: '',
+  icon: '',
+  path: '',
+  component: 'Layout',
+  is_cache: 0,
+  sort: 1
+}
+export default {
+  name: 'Menus',
+  data() {
+    return {
+      menuQuery: Object.assign({}, menuQuery),
+      tableKey: 0,
+      list: [],
+      defaultProps: {
+        children: 'children',
+        label: 'title'
+      },
+      textMap: [
+        'create',
+        'update'
+      ],
+      dialogStatus: 'create',
+      defaultMenuPid: [],
+      menuData: Object.assign({}, defaultMenuData),
+      rules: {
+        name: [
+          { required: true, message: 'name 必填', tigger: true },
+          { max: 100, message: 'name 字符不能大于100' }
         ],
-        dialogStatus: 'create',
-        defaultMenuPid: [],
-        menuData: Object.assign({}, defaultMenuData),
-        rules: {
-          name: [
-            { required: true, message: 'name 必填', tigger: true },
-            { max: 100, message: 'name 字符不能大于100' }
-          ],
-          title: [
-            { required: true, message: '菜单标题必填', tigger: true },
-            { max: 255, message: '菜单标题长度不能大于255' }
-          ],
-          path: [
-            { required: true, message: '路由必填', tigger: true },
-            { max: 255, message: '路由长度不能大于255' }
-          ],
-          component: [
-            { required: true, message: '组件路径必填', tigger: true },
-            { max: 255, message: '组件路径长度不能大于255' }
-          ]
-        },
-        filterText: ''
+        title: [
+          { required: true, message: '菜单标题必填', tigger: true },
+          { max: 255, message: '菜单标题长度不能大于255' }
+        ],
+        path: [
+          { required: true, message: '路由必填', tigger: true },
+          { max: 255, message: '路由长度不能大于255' }
+        ],
+        component: [
+          { required: true, message: '组件路径必填', tigger: true },
+          { max: 255, message: '组件路径长度不能大于255' }
+        ]
+      },
+      filterText: ''
+    }
+  },
+  watch: {
+    filterText(val) {
+      this.$refs.tree.filter(val)
+    }
+  },
+  created() {
+    this.menuList()
+  },
+  methods: {
+    menuList() {
+      menusTree().then(response => {
+        const { data } = response
+        this.list = data
+      }).catch((error) => {
+        this.$message({
+          message: error.toString(),
+          type: 'error'
+        })
+      })
+    },
+    resetMenuData(){
+      this.menuData = Object.assign({}, defaultMenuData)
+      this.defaultMenuPid = []
+      this.$refs.menuForm.clearValidate()
+      this.$refs.menuForm.resetFields()
+    },
+    handleNodeClick(data) {
+      this.dialogStatus = 'edit'
+      this.defaultMenuPid = (data.pid === 0) ? [data.id] : [data.pid, data.id]
+      this.menuData = Object.assign({}, data)
+    },
+    filterNode(value, data) {
+      if (!value) return true
+      return data.title.indexOf(value) !== -1
+    },
+    // editMenu(data) {
+    //   this.dialogStatus = 'edit'
+    //   this.defaultMenuPid = (data.pid === 0) ? [data.id] : [data.pid, data.id]
+    //   this.menuData = Object.assign({}, data)
+    // },
+    setMenuRelation(val) {
+      // 菜单下啦选中触发
+      this.menuData.pid = val[val.length - 1] !== undefined ? val[val.length - 1] : 0
+      if (this.menuData.pid !== 0) {
+        this.$refs.menuForm.clearValidate()
+        this.menuData.component = ''
+      } else {
+        this.menuData.component = 'Layout'
       }
     },
-    watch: {
-      filterText(val) {
-        this.$refs.tree.filter(val)
-      }
-    },
-    created() {
-      this.menuList()
-    },
-    methods: {
-      menuList() {
-        menusTree().then(response => {
-          const { data } = response
-          this.list = data
-        }).catch((error) => {
+    removeMenu(node, data) {
+      this.$confirm('此操作不可恢复，确定删除菜单?', {
+        type: 'warning',
+        showCancelButton: true,
+        showConfirmButton: true
+      }).then(() => {
+        destroyMenu(data.id).then((response) => {
+          this.generateMenu()
           this.$message({
-            message: error.toString(),
+            message: '操作成功',
+            type: 'success'
+          })
+        }).catch(error => {
+          this.$message({
+            message: error,
             type: 'error'
           })
         })
-      },
-      resetMenuData(){
-        this.menuData = Object.assign({}, defaultMenuData)
-        this.defaultMenuPid = []
-        this.$refs.menuForm.clearValidate()
-        this.$refs.menuForm.resetFields()
-      },
-      handleNodeClick(data) {
-        console.log(data)
-      },
-      filterNode(value, data) {
-        if (!value) return true
-        return data.title.indexOf(value) !== -1
-      },
-      editMenu(data) {
-        this.dialogStatus = 'edit'
-        this.defaultMenuPid = (data.pid === 0) ? [data.id] : [data.pid, data.id]
-        this.menuData = Object.assign({}, data)
-      },
-      setMenuRelation(val) {
-        // 菜单下啦选中触发
-        this.menuData.pid = val[val.length - 1] !== undefined ? val[val.length - 1] : 0
-        if (this.menuData.pid !== 0) {
-          this.$refs.menuForm.clearValidate()
-          this.menuData.component = ''
-        } else {
-          this.menuData.component = 'Layout'
+      })
+    },
+    createMenu() {
+      this.$refs['menuForm'].validate((valid) => {
+          if (valid) {
+            createMenu(this.menuData).then(response => {
+              this.generateMenu()
+              this.$message({
+                message: '操作成功',
+                type: 'success'
+              })
+            }).catch(error => {
+              this.$message({
+                message: error.toString(),
+                type: 'error'
+              })
+            })
+          }
         }
-      },
-      removeMenu(node, data) {
-        this.$confirm('此操作不可恢复，确定删除菜单?', {
-          type: 'warning',
-          showCancelButton: true,
-          showConfirmButton: true
-        }).then(() => {
-          destroyMenu(data.id).then((response) => {
-            this.generateMenu()
-            this.$message({
-              message: '操作成功',
-              type: 'success'
-            })
-          }).catch(error => {
-            this.$message({
-              message: error,
-              type: 'error'
-            })
-          })
-        })
-      },
-      createMenu() {
-        this.$refs['menuForm'].validate((valid) => {
-            if (valid) {
-              createMenu(this.menuData).then(response => {
-                this.generateMenu()
-                this.$message({
-                  message: '操作成功',
-                  type: 'success'
-                })
-              }).catch(error => {
-                this.$message({
-                  message: error.toString(),
-                  type: 'error'
-                })
+      )
+    },
+    updateData() {
+      this.$refs['menuForm'].validate((valid) => {
+          if (valid) {
+            updateMenu(this.menuData).then(response => {
+              const { tree } = response.data
+              this.list = tree
+              this.menuData = Object.assign({}, defaultMenuData)
+              this.dialogStatus = 'create'
+              this.defaultMenuPid = []
+              this.$refs['menuForm'].resetFields()
+              this.generateMenu()
+              this.$message({
+                message: '操作成功',
+                type: 'success'
               })
-            }
-          }
-        )
-      },
-      updateData() {
-        this.$refs['menuForm'].validate((valid) => {
-            if (valid) {
-              updateMenu(this.menuData).then(response => {
-                const { tree } = response.data
-                this.list = tree
-                this.menuData = Object.assign({}, defaultMenuData)
-                this.dialogStatus = 'create'
-                this.defaultMenuPid = []
-                this.$refs['menuForm'].resetFields()
-                this.generateMenu()
-                this.$message({
-                  message: '操作成功',
-                  type: 'success'
-                })
-              }).catch(error => {
-                this.$message({
-                  message: error.toString(),
-                  type: 'error'
-                })
+            }).catch(error => {
+              this.$message({
+                message: error.toString(),
+                type: 'error'
               })
-            }
+            })
           }
-        )
-      },
-      menuInfo() {
-        menuInfo()
-      },
-      generateMenu() {
-        this.$store.dispatch('permission/generateRoutes')
-      }
+        }
+      )
+    },
+    menuInfo() {
+      menuInfo()
+    },
+    generateMenu() {
+      this.$store.dispatch('permission/generateRoutes')
     }
   }
+}
 </script>
 
 <style scoped lang="scss">
